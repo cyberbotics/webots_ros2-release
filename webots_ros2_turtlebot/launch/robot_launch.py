d7f0dd7e4e423f85b9488d8524e2bc21074b1c0d
Darko Lukic <lukicdarkoo@gmail.com>
2020-12-01 14:22:39 +0100

    Imported upstream version '1.0.3' of 'upstream'

--- webots_ros2_turtlebot/launch/robot_launch.py
+++ webots_ros2_turtlebot/launch/robot_launch.py
@@ -14,45 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Launch Webots and the controller."""
+"""Launch Webots TurtleBot3 Burger driver."""
 
 import os
-
-import launch
-import launch_ros.actions
-
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    package_dir = get_package_share_directory('webots_ros2_tiago')
+    package_dir = get_package_share_directory('webots_ros2_turtlebot')
+    world = LaunchConfiguration('world')
 
     webots = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('webots_ros2_core'), 'launch', 'robot_launch.py')
         ),
-        launch_arguments={
-            'executable': 'webots_differential_drive_node',
-            'world': os.path.join(package_dir, 'worlds', 'ros_tiago.wbt'),
-            'node_parameters': os.path.join(package_dir, 'resource', 'tiago.yaml'),
-        }.items()
+        launch_arguments=[
+            ('package', 'webots_ros2_turtlebot'),
+            ('executable', 'turtlebot_driver'),
+            ('world', PathJoinSubstitution([package_dir, 'worlds', world])),
+        ]
     )
 
-    # Rviz node
-    use_rviz = launch.substitutions.LaunchConfiguration('rviz', default=False)
-    rviz_config = os.path.join(get_package_share_directory('webots_ros2_tiago'), 'resource', 'odometry.rviz')
-    rviz = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        output='screen',
-        arguments=['--display-config=' + rviz_config],
-        condition=launch.conditions.IfCondition(use_rviz)
-    )
-
-    return launch.LaunchDescription([
-        webots,
-        rviz
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='turtlebot3_burger_example.wbt',
+            description='Choose one of the world files from `/webots_ros2_turtlebot/world` directory'
+        ),
+        webots
     ])
